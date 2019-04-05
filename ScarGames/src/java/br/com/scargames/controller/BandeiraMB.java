70ed763890e35a21e9aa4fe4beb5a8e1096c6e41
Saulo Grego <saulogrego@gmail.com>
2019-04-05 18:56:58 -0300

    CRUD de Bandeira finalizado

--- ScarGames/src/java/br/com/scargames/controller/BandeiraMB.java
+++ ScarGames/src/java/br/com/scargames/controller/BandeiraMB.java
@@ -6,16 +6,18 @@ import java.io.Serializable;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @ManagedBean(name = "bandeiraMB")
 @ViewScoped
 public class BandeiraMB implements Serializable{
 
-    private Bandeira bandeira;
+    private Bandeira bandeira = new Bandeira();
     private List<Bandeira> bandeiras;
     
     public BandeiraMB() {
-        this.listar();
     }
     
     public void listar(){
@@ -30,6 +32,37 @@ public class BandeiraMB implements Serializable{
     public String inserir(){
         BandeiraService service = new BandeiraService();
         if (service.inserir(bandeira)){
+            return "list.xhtml?faces-redirect=true";
+        }else{
+            return null;
+        }
+    }
+    
+    public String alterar(){
+        BandeiraService service = new BandeiraService();
+        if (service.alterar(bandeira)){
+            return "list.xhtml?faces-redirect=true";
+        }else{
+            return null;
+        }
+    }
+    
+    public void recuperarBandeira(){
+        HttpServletRequest servReq = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        HttpSession session = servReq.getSession(true);
+        this.bandeira = (Bandeira)session.getAttribute("bandeira");
+    }
+    
+    public String carregarDados(Bandeira bandeira){
+        HttpServletRequest servReq = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        HttpSession session = servReq.getSession(true);
+        session.setAttribute("bandeira", bandeira);
+        return "alter.xhtml?faces-redirect=true";
+    }
+    
+    public String excluir(Bandeira bandeira){
+        BandeiraService service = new BandeiraService();
+        if (service.excluir(bandeira)){
             return "list.xhtml?faces-redirect=true";
         }else{
             return null;
